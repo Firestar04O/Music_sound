3cd9c1c378e5baeae25a29b60f550e0721df207a
Firestar04O <ricardo.espino.b@tecsup.edu.pe>
2025-04-25 14:11:55 -0500

    first point(movement)

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player : MonoBehaviour
+{
+    private Rigidbody myrigidbody;
+    private float horizontal;
+    private float transversal;
+    public int speed;
+    private void Awake()
+    {
+        myrigidbody = GetComponent<Rigidbody>();
+    }
+    private void FixedUpdate()
+    {
+        myrigidbody.velocity = new Vector3(horizontal, 0, transversal);
+    }
+    public void OnMovementX(InputAction.CallbackContext context)
+    {
+        horizontal = context.ReadValue<float>();
+    }
+    public void OnMovementY(InputAction.CallbackContext context)
+    {
+        transversal = context.ReadValue<float>();
+    }
+    public void OnJumping()
+    {
+
+    }
+}
